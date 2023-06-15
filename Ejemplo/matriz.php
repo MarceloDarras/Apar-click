71bfa9e4ae6fb4059d30d7733ecbea119646bd24
Marcelo Darras <75511290+MarceloDarras@users.noreply.github.com>
2023-06-15 15:34:44 -0400

    Add files via upload

--- Ejemplo/matriz.php
+++ Ejemplo/matriz.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link href = "CSS/formulario_diario.css" rel = "stylesheet">
+    <link href = "CSS/matriz-style.css" rel = "stylesheet">
+    <link href = "CSS/estilos.css" rel = "stylesheet">
     <title>Document</title>
 </head>
 <body>
@@ -14,20 +16,36 @@
     </div>
     <br>
 
-    <form action="" class="informe" id="informe">
+    <div class="contenedor">
+        <form action="" id="formulario" class="formulario">
+            <button class="btn" id="link" onclick="voton()">Arrendar</button>
+        </form>
+        <div id="contenedorQR" class="contenedorQR"></div>
+    </div>
+
+    <form action="controlador_registro_formularios.php" class="informe" method = "POST" id="informe">
         <h1 class="titulo">Formulario de arriendos diarios</h1>
         <div class="container">
             <div class="informe_cantidad">
-                <label for="" class="label" id="label1"></label>
+                <label for="" class="label" id="label1" name = "label_cantidad"></label>
+                <input type="text" id="input_cantidad" name = "cantidad">
+                <div class="boton_guardar">
+                    <input type="submit">
+                </div>
             </div>
+            <br>
             <br>
             <div class="informe_cantidad">
                 <label for="" class="label"></label>
             </div>
+            <?php
+                include("controlador_registro_formularios.php");
+            ?>
         </div>
     </form>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
-    <script src="JS/matrices.js"></script>
+    <script src= "JS/matrices.js"></script>
+    <script src="JS/app.js"></script>
 </body>
 </html>
